1edf06face439dfa156cb74f8210153471ea9a36
Matthew Snelgrove <matthew.s13542@gmail.com>
2024-11-04 11:36:42 -0500

    add configs

---
+++ backend/configServices/sessionConfig.ts
@@ -0,0 +1,23 @@
+import session from "express-session";
+import { pool } from "./dbConfig.js";
+import pgSession from "connect-pg-simple";
+import * as dotenv from "dotenv";
+dotenv.config({ path: `.env.${process.env.NODE_ENV || "development"}` });
+const isLocalServer = process.env.IS_LOCAL_SERVER === "true";
+const store = new (pgSession(session))({
+  pool: pool,
+});
+export const sessionMiddleware = session({
+  store: store,
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  rolling: true,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    sameSite: isLocalServer ? "strict" : "none",
+    secure: "auto",
+    partitioned: true,
+    maxAge: 1000 * 60 * 60 * 24, //24 hours, reset on activity
+  },
+});
