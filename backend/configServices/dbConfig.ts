3853740030db88a555292afb6737b9172509b3f4
Matthew Snelgrove <matthew.s13542@gmail.com>
2024-11-13 00:04:15 -0500

    parse db port env var to int

--- backend/configServices/dbConfig.ts
+++ backend/configServices/dbConfig.ts
@@ -4,6 +4,6 @@ export const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: parseInt(process.env.DB_PORT),
   database: process.env.DB_DATABASE,
 });
