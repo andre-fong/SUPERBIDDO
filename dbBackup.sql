d7310dc27fa58c1ee7b2f9718a7139bd19dd2c3f
Matthew Snelgrove <matthew.s13542@gmail.com>
2024-11-15 14:36:01 -0500

    modify db for non-oauth account

--- dbBackup.sql
+++ dbBackup.sql
@@ -84,6 +84,9 @@ $function$
 
 CREATE TABLE public.account (
 	account_id uuid NOT NULL DEFAULT gen_random_uuid(),
+	username varchar(30) NOT NULL,
+	email public.citext NOT NULL,
+	passhash bpchar(60) NOT NULL,
 	CONSTRAINT account_pk PRIMARY KEY (account_id)
 );
 
