9dcf92bdd835c6d1656804ba4589535d3e9aab0a
Matthew Snelgrove <matthew.s13542@gmail.com>
2024-11-19 00:02:50 -0500

    change session response to account instead of cookie and accountId

--- backend/routes/session.ts
+++ backend/routes/session.ts
@@ -11,15 +11,26 @@ router.get("/", async (req, res, next) => {
   if (!req.session.accountId) {
     throw sessionNotFound();
   }
-  req.session.touch();
-  res.json(req.session);
+
+  const accountRecord = camelize(
+    await pool.query<Omit<AccountDb, "passhash">>(
+      `SELECT account_id, email, username
+      FROM account
+      WHERE account_id=$1`,
+      [req.session.accountId]
+    )
+  ).rows[0];
+
+  const account: Account = accountRecord;
+
+  res.json(account);
 });
 
 router.post("/", async (req, res, next) => {
   const sessionInput: Omit<AccountInput, "username"> = req.body;
   const { email, password } = sessionInput;
 
-  const account = camelize(
+  const accountRecord = camelize(
     await pool.query<AccountDb>(
       `SELECT *
       FROM account
@@ -29,17 +40,24 @@ router.post("/", async (req, res, next) => {
   ).rows[0];
 
   // no account with email
-  if (!account) {
+  if (!accountRecord) {
     throw invalidLogin();
   }
 
   // wrong password
-  if (!(await bcrypt.compare(password, account.passhash))) {
+  if (!(await bcrypt.compare(password, accountRecord.passhash))) {
     throw invalidLogin();
   }
 
-  req.session.accountId = account.accountId;
-  res.status(201).json(req.session);
+  req.session.accountId = accountRecord.accountId;
+
+  const account: Account = {
+    accountId: accountRecord.accountId,
+    email: accountRecord.email,
+    username: accountRecord.username,
+  };
+
+  res.status(201).json(account);
 });
 
 router.delete("/", async (req, res, next) => {
