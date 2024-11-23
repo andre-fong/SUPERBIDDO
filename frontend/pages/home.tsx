4aeac6436d4a26c7dd051b648a82f3a73ff49229
Andre Fong <fongandre123@gmail.com>
2024-11-23 02:27:22 -0500

    feat: Rotating banner images

--- frontend/pages/home.tsx
+++ frontend/pages/home.tsx
@@ -1,10 +1,11 @@
 import { ErrorType } from "@/types/errorTypes";
 import { PageName } from "@/types/pageTypes";
 import { User } from "@/types/userTypes";
-import React from "react";
+import { useState, useEffect } from "react";
 import styles from "@/styles/home.module.css";
 import Image from "next/image";
 import Button from "@mui/material/Button";
+import { AnimatePresence, motion } from "motion/react";
 
 export default function Home({
   setCurPage,
@@ -17,38 +18,179 @@ export default function Home({
   setToast: (err: ErrorType) => void;
   context: string;
 }) {
+  const [heroIndex, setHeroIndex] = useState(0);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setHeroIndex((prev) => (prev + 1) % 3);
+    }, 5000);
+
+    return () => clearTimeout(timeout);
+  }, [heroIndex]);
+
   return (
     <>
       <div className={styles.hero_container}>
-        <div className={styles.hero_banner}>
-          <Image
-            src="/pokemon-banner-1.jpg"
-            alt="Pokemon Banner"
-            fill
-            priority
-            style={{
-              objectFit: "cover",
-              objectPosition: "-20px center",
-              transform: "scaleX(-1)",
-            }}
-          />
-          <div className={styles.hero_text}>
-            <h2 className={styles.hero_title}>Pokémon</h2>
-            <p className={styles.hero_subtitle}>Cards, bundles, and more</p>
-            <Button
-              variant="contained"
-              sx={{
-                width: "fit-content",
-                fontWeight: 600,
+        <AnimatePresence initial={false} mode="popLayout">
+          {heroIndex === 0 && (
+            <motion.div
+              key="pokemon"
+              className={styles.hero_banner}
+              initial={{
+                x: "100vw",
               }}
-              onClick={() =>
-                setCurPage("results", JSON.stringify({ category: "pokemon" }))
-              }
+              animate={{
+                x: 0,
+              }}
+              exit={{
+                x: "-100vw",
+              }}
+              transition={{
+                type: "spring",
+                duration: 0.4,
+              }}
             >
-              View Auctions
-            </Button>
-          </div>
-        </div>
+              <Image
+                src="/pokemon-banner-1.jpg"
+                alt="Pokemon Banner"
+                fill
+                priority
+                style={{
+                  objectFit: "cover",
+                  objectPosition: "-20px center",
+                  transform: "scaleX(-1)",
+                }}
+              />
+              <div className={styles.hero_text}>
+                <h2 className={styles.hero_title}>Pokémon</h2>
+                <p className={styles.hero_subtitle}>Cards, bundles, and more</p>
+                <Button
+                  variant="contained"
+                  sx={{
+                    width: "fit-content",
+                    fontWeight: 600,
+                  }}
+                  onClick={() =>
+                    setCurPage(
+                      "results",
+                      JSON.stringify({ category: "pokemon" })
+                    )
+                  }
+                >
+                  View Auctions
+                </Button>
+              </div>
+            </motion.div>
+          )}
+
+          {heroIndex === 1 && (
+            <motion.div
+              key="mtg"
+              className={styles.hero_banner}
+              initial={{
+                x: "100vw",
+              }}
+              animate={{
+                x: 0,
+              }}
+              exit={{
+                x: "-100vw",
+              }}
+              transition={{
+                type: "spring",
+                duration: 0.4,
+              }}
+            >
+              <Image
+                src="/mtg-banner-2.jpg"
+                alt="Magic: The Gathering Banner"
+                fill
+                priority
+                style={{
+                  objectFit: "cover",
+                  objectPosition: "center",
+                }}
+              />
+              <div className={styles.hero_text}>
+                <h2 className={styles.hero_title}>Magic: The Gathering</h2>
+                <p
+                  className={styles.hero_subtitle}
+                  style={{ color: "#ff5e45" }}
+                >
+                  Cards, bundles, and more
+                </p>
+                <Button
+                  variant="contained"
+                  color="warning"
+                  sx={{
+                    width: "fit-content",
+                    fontWeight: 600,
+                  }}
+                  onClick={() =>
+                    setCurPage("results", JSON.stringify({ category: "mtg" }))
+                  }
+                >
+                  View Auctions
+                </Button>
+              </div>
+            </motion.div>
+          )}
+
+          {heroIndex === 2 && (
+            <motion.div
+              key="yugioh"
+              className={styles.hero_banner}
+              initial={{
+                x: "100vw",
+              }}
+              animate={{
+                x: 0,
+              }}
+              exit={{
+                x: "-100vw",
+              }}
+              transition={{
+                type: "spring",
+                duration: 0.4,
+              }}
+            >
+              <Image
+                src="/yugioh-banner-2.jpg"
+                alt="Yu-Gi-Oh! Banner"
+                fill
+                priority
+                style={{
+                  objectFit: "cover",
+                  objectPosition: "center",
+                }}
+              />
+              <div className={styles.hero_text}>
+                <h2 className={styles.hero_title}>Yu-Gi-Oh!</h2>
+                <p
+                  className={styles.hero_subtitle}
+                  style={{ color: "lightgray" }}
+                >
+                  Cards, bundles, and more
+                </p>
+                <Button
+                  variant="contained"
+                  color="info"
+                  sx={{
+                    width: "fit-content",
+                    fontWeight: 600,
+                  }}
+                  onClick={() =>
+                    setCurPage(
+                      "results",
+                      JSON.stringify({ category: "yugioh" })
+                    )
+                  }
+                >
+                  View Auctions
+                </Button>
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
 
       <div className={styles.section_container}>
