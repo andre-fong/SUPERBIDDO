412a47419129a1d4d4807974bb4e08cddbff53a2
Matthew Snelgrove <matthew.s13542@gmail.com>
2024-11-29 20:45:04 -0500

    add put address

--- backend/routes/accounts.ts
+++ backend/routes/accounts.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { pool } from "../configServices/dbConfig.js";
 import camelize from "camelize";
 import bcrypt from "bcrypt";
-import { BusinessError, sessionNotFound } from "../utils/errors.js";
+import { BusinessError, unauthorized } from "../utils/errors.js";
 
 export const router = express.Router();
 
@@ -55,4 +55,65 @@ router.post("/", async (req, res) => {
   const account = await createAccount(email, passhash, username);
 
   res.status(201).json(account);
+});
+
+router.put("/:accountId/address", async (req, res) => {
+  const { accountId } = req.params;
+
+  if (req.session.accountId !== accountId) {
+    throw unauthorized();
+  }
+
+  const { placeId, sessionToken } = req.body;
+  const fields = "formatted_address,geometry";
+  const gmapsResponse = await fetch(
+    `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=${fields}&sessiontoken=${sessionToken}&key=${process.env.GOOGLE_MAPS_API_KEY}`,
+    {
+      method: "GET",
+    }
+  );
+  const gmapsJson = await gmapsResponse.json();
+  if (gmapsJson.status !== "OK") {
+    throw new BusinessError(400, "Invalid place ID");
+  }
+  const { formatted_address: addressFormatted, geometry } =
+    gmapsJson.result as {
+      formatted_address: string;
+      geometry: { location: { lat: number; lng: number } };
+      status: "OK";
+    };
+
+  const accountRecord = camelize(
+    await pool.query<
+      AccountDb & {
+        address_formatted: string;
+        latitude: number;
+        longitude: number;
+      }
+    >(
+      ` UPDATE account
+        SET address_formatted=$1, latitude=$2, longitude=$3
+        WHERE account_id=$4
+        RETURNING account_id, email, username, address_formatted, latitude, longitude`,
+      [
+        addressFormatted,
+        geometry.location.lat,
+        geometry.location.lng,
+        accountId,
+      ]
+    )
+  ).rows[0];
+
+  // account should exist if session is valid
+  if (!accountRecord) {
+    throw new BusinessError(500, "Error updating address");
+  }
+
+  const account: Account & {
+    addressFormatted: string;
+    latitude: number;
+    longitude: number;
+  } = accountRecord;
+
+  res.json(account);
 });
